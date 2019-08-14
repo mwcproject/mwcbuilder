76cb2ae19af0b2935c50d05fad76eeafcddcf321
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-08-14 19:11:33 +0000

    add qt5

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,7 +4,7 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-6.0 main"
 
 sudo apt update
-sudo apt install clang-6.0 git curl make build-essential libgl1-mesa-dev openssl libssl1.0-dev -y
+sudo apt install qt5-default qtcreator clang-6.0 git curl make build-essential libgl1-mesa-dev openssl libssl1.0-dev -y
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 
