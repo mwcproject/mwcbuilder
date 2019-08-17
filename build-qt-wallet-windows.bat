a0d137d97dd53fdf05f6eb62572156610bfd8b12
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-08-17 04:49:03 +0100

    update script

--- build-qt-wallet-windows.bat
+++ build-qt-wallet-windows.bat
@@ -1,27 +1,29 @@
 
-# clean dirs
-del /s /q mwc713 mwc-qt-wallet target
+del /s /q target
+rmdir /s /q target
+del /s /q mwc713
+rmdir /s /q mwc713
+del /s /q mwc-qt-wallet
+rmdir /s /q mwc-qt-wallet
+
 mkdir target
 
-# Build mwc713
 git clone https://github.com/mwcproject/mwc713
 cd mwc713
 cargo build --release
 cd ..
 
 
-# Build mwc-qt-wallet
 git clone https://github.com/mwcproject/mwc-qt-wallet
 cd mwc-qt-wallet
 qmake -spec mingw73_64 mwc-qt-wallet.pro
 make
 cd ..
 
-# Copy files over to build installers
-cp -rp nsis target
+xcopy nsis target/
 
-cp mwc713/target/release/mwc713.exe target/nsis/payload
-cp mwc-qt-wallet/release/mwc-qt-wallet.exe target/nsis/payload
+xcopy mwc713/target/release/mwc713.exe target/nsis/payload
+xcopy mwc-qt-wallet/release/mwc-qt-wallet.exe target/nsis/payload
 
 cd target/nsis
 makensis x64.nsi
