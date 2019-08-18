bab7c26fdea39fbf3b9e4e8070bdc1dd9655d4e6
cgilliard <christopher.gilliard@gmail.com>
2019-08-18 01:09:46 -0700

    fix deploy

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-scp target/*.deb uploader@ftp.mwc.mw:/home/uploader/mwc-qt-wallet_`date | cut -d ' ' -f2`_`date | cut -d ' ' -f3`_`date | cut -d ' ' -f6`.deb;
+
+echo "md5sum = `md5 target/*.deb`";
+./sendfile.expect target/*.deb mwc-qt-wallet_`date | cut -d ' ' -f2`_`date | cut -d ' ' -f3`_`date | cut -d ' ' -f6`.deb;
