91e773917a8f1f048e102c0c9cd1ac3d82b477de
cgilliard <christopher.gilliard@gmail.com>
2019-08-18 01:37:58 -0700

    fix md5sum and expect deps

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 
-echo "md5sum = `md5 target/*.deb`";
+echo "md5sum = `md5sum target/*.deb`";
 ./sendfile.expect target/*.deb mwc-qt-wallet_`date | cut -d ' ' -f2`_`date | cut -d ' ' -f3`_`date | cut -d ' ' -f6`.deb;
--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,6 +4,7 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-6.0 main"
 
 sudo apt update
+sudo apt-get install expect
 sudo apt install clang-6.0 git curl make build-essential libgl1-mesa-dev openssl libssl1.0-dev -y
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
