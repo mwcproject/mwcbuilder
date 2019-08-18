4fb081f0618ba2ae16619aa734b7abd04cb350a5
cgilliard <christopher.gilliard@gmail.com>
2019-08-18 07:24:31 -0700

    bypass known hosts question

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -2,4 +2,5 @@
 
 
 echo "md5sum = `md5sum target/*.deb`";
+echo "ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM=" >> ~/.ssh/known_hosts
 ./sendfile.expect target/*.deb mwc-qt-wallet_`date | cut -d ' ' -f2`_`date | cut -d ' ' -f3`_`date | cut -d ' ' -f6`.deb;
