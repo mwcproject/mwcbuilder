da5b24cb66a970e91b2e5c85f92f27a7ef960cde
cgilliard <christopher.gilliard@gmail.com>
2019-08-18 11:27:35 -0700

    consistent date formatting in linux

--- deploy_linux.sh
+++ deploy_linux.sh
@@ -4,4 +4,5 @@
 echo "md5sum = `md5sum target/*.deb`";
 mkdir -p ~/.ssh
 echo "ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM=" >> ~/.ssh/known_hosts
-./sendfile.expect target/*.deb mwc-qt-wallet_`date | cut -d ' ' -f2`_`date | cut -d ' ' -f3`_`date | cut -d ' ' -f6`.deb;
+DATE=`date +"%m-%d-%y"`
+./sendfile.expect target/*.deb mwc-qt-wallet-1.0.5-linux64-$DATE.deb;
