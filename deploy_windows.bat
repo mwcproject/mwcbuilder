1f16be46345d648d8cb891a7cce318bf3c66bd36
cgilliard <christopher.gilliard@gmail.com>
2019-08-18 08:45:31 -0700

    take out semicolons for windows

--- deploy_windows.bat
+++ deploy_windows.bat
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-echo "md5sum = `md5sum target/nsis/mwc-qt-wallet*.exe`";
+echo "md5sum = `md5sum target/nsis/mwc-qt-wallet*.exe`"
 mkdir -p ~/.ssh
 echo "ftp.mwc.mw ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFCzEhIbZcESW50l2Mh9dFIeObKrDBNwZm+FPZzL3tp7U8xkcH0U7rx87cMDUKUfJnO8soJ3yqxf1RXOrFkXKQM=" >> ~/.ssh/known_hosts
-sendfile.expect target/nsis/mwc-qt-wallet*.exe mwc-qt-wallet_`date | cut -d ' ' -f2`_`date | cut -d ' ' -f3`_`date | cut -d ' ' -f6`.exe;
+sendfile.expect target/nsis/mwc-qt-wallet*.exe mwc-qt-wallet_`date | cut -d ' ' -f2`_`date | cut -d ' ' -f3`_`date | cut -d ' ' -f6`.exe
