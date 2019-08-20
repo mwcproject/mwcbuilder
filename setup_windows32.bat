5a9617b20dd96d1cc914732d2141e303c497bb69
cgilliard <christopher.gilliard@gmail.com>
2019-08-20 15:24:56 -0700

    optimize space

--- setup_windows32.bat
+++ setup_windows32.bat
@@ -1,18 +1,8 @@
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0"
-
-echo Check operating system ...
-if defined PROGRAMFILES(X86) (
-    echo 64-bit sytem detected
-) else (
-    echo 32-bit sytem detected
-)
+df -h
 
 git clone https://github.com/mwcproject/mwcbuilder-win32-helpers
-cat mwcbuilder-win32-helpers/qt5/* > Qt5.tar.bz2
-bzip2 -dc Qt5.tar.bz2 | tar xvf -
 
-cat mwcbuilder-win32-helpers/qt5-part2/* > Qt5p2.tar.bz2
-bzip2 -dc Qt5p2.tar.bz2 | tar xvf -
+cat qt5-part2/* | bzip2 -dc | tar xvf -
+cat qt5/* | bzip2 -dc | tar xvf -
+rm -rf mwcbuilder-win32-helpers
 
-
-rm -rf Qt5.tar.bz2 Qt5p2.tar.bz2
