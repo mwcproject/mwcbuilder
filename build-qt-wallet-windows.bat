094cf9a80c8e686dca39479c7d1e3596affbaca1
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-08-19 19:17:57 -0700

    win32

--- build-qt-wallet-windows.bat
+++ build-qt-wallet-windows.bat
@@ -5,8 +5,8 @@ del /s /q mwc713
 rmdir /s /q mwc713
 del /s /q mwc-qt-wallet
 rmdir /s /q mwc-qt-wallet
-del /s /q libs
-rmdir /s /q libs
+del /s /q lib
+rmdir /s /q lib
 
 bzip2 -dc libs.tar.bz2 | tar xvf -
 set LIBCLANG_PATH=%cd%\lib
---
+++ setup_windows32.bat
@@ -0,0 +1,1 @@
+bzip2 -dc C:\Users\chris\Qt5.tar.bz2 | tar xvf -
