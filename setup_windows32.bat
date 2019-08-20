d2fe857ed611fc43a78b86c435887c2e622c9181
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-08-19 23:57:01 -0700

    update toolchain rust

--- setup_windows32.bat
+++ setup_windows32.bat
@@ -1,3 +1,4 @@
+rustup install stable-i686-pc-windows-msvc
 git clone https://github.com/mwcproject/mwcbuilder-win32-helpers
 cat mwcbuilder-win32-helpers/qt5/* > Qt5.tar.bz2
 bzip2 -dc Qt5.tar.bz2 | tar xvf -
