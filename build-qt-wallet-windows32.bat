671b8a4a5017fcda5f2fb8e156b2581e1a86acc4
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-08-20 12:57:07 -0700

    just linker not compiler

--- build-qt-wallet-windows32.bat
+++ build-qt-wallet-windows32.bat
@@ -21,8 +21,6 @@ cd mwc-qt-wallet
 
 sed -e 's/^LINKER .*/LINKER = D:\/a\/1\/s\/Qt\/Tools\/mingw730_32\/bin\/g++/' Makefile.Release > Makefile.Release.1
 
-sed -e 's/^CXX .*/CXX = D:\/a\/1\/s\/Qt\/Tools\/mingw730_32\/bin\/g++/' Makefile.Release.1 > Makefile.Release
-
 perl -pi -e 's/\r\n/\n/g' Makefile.Release
 
 rm -rf Makefile.Release.1
