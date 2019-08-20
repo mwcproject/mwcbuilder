3d69958a1dc05e6b620a192f5db527bd0f3d13f4
cgilliard <christopher.gilliard@gmail.com>
2019-08-20 15:29:52 -0700

    correct path

--- setup_windows32.bat
+++ setup_windows32.bat
@@ -2,7 +2,7 @@ df -h
 
 git clone https://github.com/mwcproject/mwcbuilder-win32-helpers
 
-cat qt5-part2/* | bzip2 -dc | tar xvf -
-cat qt5/* | bzip2 -dc | tar xvf -
+cat mwcbuilder-win32-helpers/qt5-part2/* | bzip2 -dc | tar xvf -
+cat mwcbuilder-win32-helpers/qt5/* | bzip2 -dc | tar xvf -
 rm -rf mwcbuilder-win32-helpers
 
