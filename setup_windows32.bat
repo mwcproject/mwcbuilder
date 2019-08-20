c3bfeb760550336b33906f76df1228ef77a4e6ca
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-08-19 23:42:56 -0700

    win32 builds

--- setup_windows32.bat
+++ setup_windows32.bat
@@ -1,1 +1,4 @@
-bzip2 -dc C:\Users\chris\Qt5.tar.bz2 | tar xvf -
+git clone https://github.com/mwcproject/mwcbuilder-win32-helpers
+cat mwcbuilder-win32-helpers/qt5/* > Qt5.tar.bz2
+bzip2 -dc Qt5.tar.bz2 | tar xvf -
+rm -rf Qt5.tar.bz2
