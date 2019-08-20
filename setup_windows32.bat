e5b50f531f9f16c2df3653dec6714e399bc5e93b
cgilliard <christopher.gilliard@gmail.com>
2019-08-20 09:51:06 -0700

    more env info

--- setup_windows32.bat
+++ setup_windows32.bat
@@ -1,5 +1,12 @@
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0"
 
+echo Check operating system ...
+if defined PROGRAMFILES(X86) (
+    echo 64-bit sytem detected
+) else (
+    echo 32-bit sytem detected
+)
+
 rustup install stable-i686-pc-windows-msvc
 git clone https://github.com/mwcproject/mwcbuilder-win32-helpers
 cat mwcbuilder-win32-helpers/qt5/* > Qt5.tar.bz2
