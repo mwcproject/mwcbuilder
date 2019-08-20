0ec0ba1324d6cd392b030ae13b4473d569124217
Christopher Gilliard <christopher.gilliard@gmail.com>
2019-08-19 23:30:18 -0700

    static

--- build-qt-wallet-windows.bat
+++ build-qt-wallet-windows.bat
@@ -21,7 +21,7 @@ cd ..
 
 git clone https://github.com/mwcproject/mwc-qt-wallet
 cd mwc-qt-wallet
-..\Qt\5.13.0\mingw73_64\bin\qmake -spec win32-g++ mwc-qt-wallet.pro
+..\Qt\5.13.0\mingw73_64\bin\qmake -spec win32-g++ mwc-qt-wallet.pro QMAKE_LFLAGS += -static
 make
 cd ..
 
